bf95e76681ed9b256084aea3ac7c27f59668c128
MattClayton3 <mats.claesson3@gmail.com>
2025-03-17 16:26:15 +0100

    Nytt försök
    Skam den som ger sig.

--- tests/ranta2.spec.ts
+++ tests/ranta2.spec.ts
@@ -913,7 +913,7 @@ test('Sorterat', async () => {
   console.log(bankarr);
 
   let exempelBelopp = 100000;
-  let outputFile = 'index333.html';
+  let outputFile = 'index.html';
   fs.writeFileSync(outputFile, '<!DOCTYPE html>\n');
   fs.appendFileSync(outputFile, '<html>\n');
   fs.appendFileSync(outputFile, '<head>\n');
